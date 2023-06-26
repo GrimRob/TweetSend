064638e748bf24eb95719c09015420beabf5d7cf
GrimRob <rsedgwick@grimsbyfans.com>
2023-06-26 20:48:03 +0100

    Example1 added in Program1

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # TweetSend
-C# example of how to send a Tweet with https://api.twitter.com/2/tweets
+
+This is C# example of how to send a Tweet with the [Twitter API](https://api.twitter.com/2/tweets).
+
+The first example does not use media.
+
+# Preconditions
+
+You need to login in to Twitter and create a Project and App at the [Google Developer Dashboard](https://developer.twitter.com/en/portal/dashboard).
+
+Once that's set up (you may need to keep trying) then you need to generate an Api Key and Secret. You also need a writeable Access Token and Secret (you don't need a Bearer Token for this). The make a writeable token you will need to edit your `User authentication settings` (even though there is no user) and choose `Read and write` for your `App permissions`.
+
+You should have four strings which look something like this:
+
+```
+private const string APIKey = "AhbgeX6MnPN9CWzueX58BEVSr";
+private const string APISecret = "NEdzKiRWcrpmdXUyBhVoqUuAkRKqYFNg5E34qg2ZSt8yb5xA4q";
+private const string AccessToken = "6726397748-n8rzzDrmYh69rQ7Wmhj6rgSFY8E9ZTZN1WY5NZY";
+private const string AccessTokenSecret = "Wj5TWJq1AXTxM31jkuf2sYXQg3bF1Dp3E95Chq3zP9MUP";
+```
+
+Replace my example (not a real set of keys) with your own and try the examples.
+
+# Examples
+
+1. [No media](TweetSend/Program1.cs).
---
+++ TweetSend/Program1.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using OAuth;
+using System.Text;
+
+internal class Program1
+{
+    // TODO - store these somewhere safe
+    private const string APIKey = "AhbgeX6MnPN9CWzueX58BEVSr";
+    private const string APISecret = "NEdzKiRWcrpmdXUyBhVoqUuAkRKqYFNg5E34qg2ZSt8yb5xA4q";
+    private const string AccessToken = "6726397748-n8rzzDrmYh69rQ7Wmhj6rgSFY8E9ZTZN1WY5NZY";
+    private const string AccessTokenSecret = "Wj5TWJq1AXTxM31jkuf2sYXQg3bF1Dp3E95Chq3zP9MUP";
+
+    private static async Task Main(string[] args)
+    {
+        var tweet = "Hello world";
+        var oauth = new OAuthMessageHandler(APIKey, APISecret, AccessToken, AccessTokenSecret);
+
+        var tweetData = new { text = tweet };
+        var jsonData = JsonConvert.SerializeObject(tweetData);
+
+        var createTweetEndpoint = "https://api.twitter.com/2/tweets";
+        var createTweetRequest = new HttpRequestMessage(HttpMethod.Post, createTweetEndpoint)
+        {
+            Content = new StringContent(jsonData, Encoding.UTF8, "application/json")
+        };
+
+        using (var httpClient = new HttpClient(oauth))
+        {
+            var response = await httpClient.SendAsync(createTweetRequest);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Tweet sent successfully!");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to send tweet. Error: {response.ReasonPhrase}");
+            }
+        }
+    }
+}
